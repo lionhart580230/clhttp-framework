03082cc68872edce3c4db41b58607872b01e8336
xiaolan <xiaolan580230@gmail.com>
2021-08-10 10:41:28 +0800

    添加header

--- core/httpserver/httpserver.go
+++ core/httpserver/httpserver.go
@@ -300,13 +300,13 @@ func uploadFile (rw http.ResponseWriter, rq *http.Request) {
 		RequestURI: rq.RequestURI,
 		Host:       rq.Host,
 		Method:     rq.Method,
+		Header:     rq.Header,
 		RequestURL: request_url,
 		UA:         myUA,
 		UAType:     UAToInt(myUA),
 		Proctol:    proctol,
 		Language:   "zh-cn",
 	}
-
 
 	var rqObj = rule.NewHttpParam(map[string] string{
 		"ac": "UploadFile",			// 执行的动作
