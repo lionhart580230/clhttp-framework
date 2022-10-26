fd82bc80482b628e8bd78c664b96e28797bc12f1
xiaolan <xiaolan580230@gmail.com>
2022-10-26 14:47:35 +0800

    v1.5.1

--- core/rule/api_general.go
+++ core/rule/api_general.go
@@ -1,6 +1,7 @@
 package rule
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/xiaolan580230/clUtil/clFile"
 	"github.com/xiaolan580230/clUtil/clLog"
@@ -20,7 +21,7 @@ import (
 	"strings"
 )
 
-
+%v
 func %v(_auth *clAuth.AuthInfo, _param *rule.HttpParam, _server *rule.ServerParam) string {
 
 	return clResponse.Success(clJson.M{
@@ -40,7 +41,7 @@ func ApiGeneral(_pathName string, _package, _request string) {
 
 		apiName := "Api" + strings.ToUpper(string(val.Name[0])) + val.Name[1:]
 		fileName := _pathName + "/" + apiName + ".go"
-		writeBuffer := fmt.Sprintf(ApiTemp, _package, apiName)
+		writeBuffer := fmt.Sprintf(ApiTemp, _package, "", apiName)
 		if clFile.FileIsExists(fileName) {
 			clLog.Info("文件: %v 已经存在, 跳过!", fileName)
 			continue
@@ -52,4 +53,107 @@ func ApiGeneral(_pathName string, _package, _request string) {
 			clLog.Info("生成文件: %v 成功!", fileName)
 		}
 	}
+}
+
+type GeneralParams struct {
+	Name string `json:"name"`
+	Strict bool `json:"strict"`
+	Type string `json:"type"`
+	Comment string `json:"comment"`
+}
+type GeneralRouterInfo struct {
+	Ac string `json:"ac"`
+	Comment string `json:"comment"`
+	Params []GeneralParams `json:"params"`
+	Login bool `json:"login"`
+}
+
+// api文件根据json
+//@param _pathName 要生成在哪个目录
+func ApiGeneralByJson(_jsonStr, _pathName string, _package, _request string, _file string) {
+
+	var data = make([]GeneralRouterInfo, 0)
+	err := json.Unmarshal([]byte(_jsonStr), &data)
+	if err != nil {
+		clLog.Info("生成API失败! 解析目标JSON出错: %v", err)
+		return
+	}
+
+	var tempFunc = `func Init() {
+
+	// 添加请求类型
+	rule.AddRequest("` + _request + `", "ac")
+
+// 接口列表
+%v
+
+}`
+
+	var tempItem = `
+	%v
+	rule.AddRule(rule.Rule{
+		Request:     "%v",
+		Name:        "%v",
+		Params:      []rule.ParamInfo{` +"\n" + `%v		},
+		CallBack:    %v,
+		CacheExpire: 0,
+		CacheType:   0,
+		Login:       %v,
+	})
+	` + "\n\n"
+	var tempParam = `			rule.NewParam("%v", rule.%v, %v, ""),` + "\n"
+
+	// 生成路由表缓冲区
+	var ruleListContent = strings.Builder{}
+
+	ruleListContent.WriteString(`package rulelist
+
+import "github.com/xiaolan580230/clhttp-framework/core/rule"
+
+`)
+
+	clFile.CreateDirIFNotExists(_pathName)
+
+	var ruleBodyBuilder = strings.Builder{}
+
+	for _, val := range data {
+
+		apiName := "Api" + strings.ToUpper(string(val.Ac[0])) + val.Ac[1:]
+		fileName := _pathName + "/" + apiName + ".go"
+		comment := ""
+		if val.Comment != "" {
+			comment = "// " + val.Comment
+		}
+		for _, p := range val.Params {
+			comment += "\n// " + p.Name + " " + p.Comment
+		}
+		writeBuffer := fmt.Sprintf(ApiTemp, _package, comment, apiName)
+		if clFile.FileIsExists(fileName) {
+			clLog.Info("文件: %v 已经存在, 跳过!", fileName)
+			continue
+		}
+
+		var paramsBuilder = strings.Builder{}
+		for _, p := range val.Params {
+			paramsBuilder.WriteString(fmt.Sprintf(tempParam, p.Name, p.Type, p.Strict))
+		}
+
+		comment = ""
+		if val.Comment != "" {
+			comment = "// " + val.Comment
+		}
+
+		ruleBodyBuilder.WriteString(fmt.Sprintf(tempItem, comment, _request, val.Ac, paramsBuilder.String(), _package + "." + apiName, val.Login))
+
+		if err := ioutil.WriteFile(fileName, []byte(writeBuffer), 0666); err != nil {
+			clLog.Error("生成Api文件: %v 失败: %v", fileName, err)
+		} else {
+			clLog.Info("生成文件: %v 成功!", fileName)
+		}
+	}
+
+	ruleListContent.WriteString(fmt.Sprintf(tempFunc, ruleBodyBuilder.String()))
+	clFile.CreateDirIFNotExists("rulelist")
+	clFile.AppendFile("rulelist/" + _file + ".go", ruleListContent.String())
+	clLog.Info("文件内容:\n\n %+v\n", ruleListContent.String())
 }
